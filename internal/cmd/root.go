de43344bbd6cc5cbf6ce2ef3a5f00787e98797e5
simonwehle <simon.wehle@gmx.net>
2026-01-24 12:04:41 +0100

    add gpx on map

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -7,6 +7,7 @@ import (
 
 func Execute() {
 	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	addr := ":8080"
 	log.Println("Server started at port" + addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
